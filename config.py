4e467c1e5ed271856b477c8ba8a18a5be86acbac
epic-designer <mohsinkhan1292002@gmail.com>
2023-09-06 00:40:33 +0530

    ownerID fix

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-OWNER_ID = [1238234357]
+OWNER_ID = os.getenv("OWNER_ID")
 HANDLER = ["~", ".","!","?","@","$"]
 PyroX = "https://te.legra.ph/file/6a839cecad51014a32254.mp4"
 GROUP_ID = os.getenv("GROUP_ID", "-1001964645198")
